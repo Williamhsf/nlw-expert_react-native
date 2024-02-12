eddd7db2e1f2f3a976f7f0408b1171e973759c5d
Williamhsf <williamhenriquefb@gmail.com>
2024-02-12 20:47:54 -0300

    aula 01

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,37 @@
-import { View, Text, StyleSheet } from "react-native"
+import { useState } from "react"
+import { View, FlatList } from "react-native"
+
+import { CATEGORIES } from "@/utils/data/products";
+
+import { Header } from '@/components/header';
+import { CategoryButton } from '@/components/category-button'
 
 export default function Home() {
+  const [category, setCategory] = useState(CATEGORIES[0])
+
+  function handleCategorySelect(selectedCategory: string) {
+    setCategory(selectedCategory)
+  }
+
   return (
-    <View style={styles.container} >
-      <Text>Hello React Native</Text>
+    <View className="flex-1 pt-8">
+      <Header title="Faça seu pedido" cartQuantityItems={3} />
+
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <CategoryButton 
+            title={item} 
+            isSelected={ item === category } 
+            onPress={() => handleCategorySelect(item)}
+          />
+        )}
+        horizontal
+        className="max-h-10 mt-5"
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+      />      
     </View>
   )
 }
-
-// criando um objeto de estilização
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "red",
-    flex: 1,
-  }
-})
